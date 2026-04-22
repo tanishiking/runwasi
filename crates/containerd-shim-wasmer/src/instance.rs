65f0cc8a4a7f82eba0f201dccade9153dbf1e7f1
Rikito Taniguchi <rikitotaniguchi@proton.me>
2026-04-22 15:11:05 +0900

    fix4

--- crates/containerd-shim-wasmer/src/instance.rs
+++ crates/containerd-shim-wasmer/src/instance.rs
@@ -1,11 +1,15 @@
+use std::sync::Arc;
+
 use anyhow::Result;
 use containerd_shim_wasm::sandbox::Sandbox;
 use containerd_shim_wasm::sandbox::context::{Entrypoint, RuntimeContext};
 use containerd_shim_wasm::shim::{Shim, Version, version};
 use tokio::runtime::Handle;
-use wasmer::{Module, Store};
+use wasmer::Module;
+use wasmer_types::ModuleHash;
+use wasmer_wasix::WasiRuntimeError;
+use wasmer_wasix::runners::wasi::{RuntimeOrEngine, WasiRunner};
 use wasmer_wasix::virtual_fs::host_fs::FileSystem;
-use wasmer_wasix::{WasiEnv, WasiError};
 
 pub struct WasmerShim;
 
@@ -46,40 +50,42 @@ impl Sandbox for WasmerSandbox {
 
         let mod_name = name.unwrap_or_else(|| "main".to_string());
 
-        log::info!("Create a Store");
-        let mut store = Store::new(self.engine.clone());
-
         let wasm_bytes = source.as_bytes()?;
-        let module = Module::from_binary(&store, &wasm_bytes)?;
+        let engine = wasmer::Engine::from(self.engine.clone());
+        let module = Module::new(&engine, &wasm_bytes)?;
 
         let runtime = tokio::runtime::Builder::new_multi_thread()
             .enable_all()
             .build()?;
         let _guard = runtime.enter();
 
-        log::info!("Creating `WasiEnv`...: args {args:?}, envs: {envs:?}");
+        log::info!("Creating `WasiRunner`...: args {args:?}, envs: {envs:?}");
         let fs = FileSystem::new(Handle::current(), "/")?;
-        let (instance, wasi_env) = WasiEnv::builder(mod_name)
-            .args(&args[1..])
-            .envs(envs)
-            .fs(Box::new(fs))
-            .preopen_dir("/")?
-            .instantiate(module, &mut store)?;
-
-        let _ = unsafe { wasi_env.bootstrap(&mut store)? };
-
         log::info!("Running {func:?}");
-        let start = instance.exports.get_function(&func)?;
-        wasi_env.data(&store).thread.set_status_running();
-        let status = tokio::task::block_in_place(|| {
-            start.call(&mut store, &[]).map(|_| 0).or_else(|err| {
-                match err.downcast_ref::<WasiError>() {
-                    Some(WasiError::Exit(code)) => Ok(code.raw()),
-                    _ => Err(err),
-                }
-            })
-        })?;
+        let mut runner = WasiRunner::new();
+        runner
+            .with_args(args.iter().skip(1).cloned())
+            .with_envs(envs)
+            .with_entry_function(func)
+            .with_mount("/".to_string(), Arc::new(fs));
 
-        Ok(status)
+        match runner.run_wasm(
+            RuntimeOrEngine::Engine(engine),
+            &mod_name,
+            module,
+            ModuleHash::xxhash(wasm_bytes.as_ref()),
+        ) {
+            Ok(()) => Ok(0),
+            Err(err) => {
+                if let Some(code) = err
+                    .downcast_ref::<WasiRuntimeError>()
+                    .and_then(WasiRuntimeError::as_exit_code)
+                {
+                    Ok(code.raw() as i32)
+                } else {
+                    Err(err)
+                }
+            }
+        }
     }
 }
